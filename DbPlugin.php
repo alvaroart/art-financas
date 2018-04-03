b8a3dc95a8c443ac6de27c81895f4c6d4438f027
Alvaro Rosa Teixeira <alvaro.rosa1985@gmail.com>
2018-04-02 23:43:30 -0300

    Add files via upload

---
+++ DbPlugin.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+namespace ARTFin\Plugins;
+
+
+use ARTFin\ServiceContainerInterface;
+use Illuminate\Database\Capsule\Manager as capsule;
+
+class DbPlugin implements PluginInterface
+{
+
+    public function register(ServiceContainerInterface $container)
+    {
+        $capsule = new Capsule();
+        $config = include __DIR__ . '/../../config/db.php';
+        $capsule->addConection($config['default']);
+        $capsule->bootEloquent();
+
+    }
+}
